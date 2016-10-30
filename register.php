8a0563f14a663461796a4dc56f8dc0551bd3b3a8
Freezy96 <stenlleycold@yahoo.com>
2016-10-30 15:43:40 +0800

    5

--- register.php
+++ register.php
@@ -4,10 +4,10 @@ require_once('config.php');
 $username = mysqli_escape_string($conn, $_POST['username']);
 $password = mysqli_escape_string($conn, md5($_POST['password']));
 $email = mysqli_escape_string($conn, $_POST['email']);
-$sql = "INSERT INTO users(username, password, email) VALUES('$username', '$password', '$email')";
+$sql = "INSERT INTO users(username, password, email, identity) VALUES('$username', '$password', '$email', 'Student')";
  $result = $conn->query($sql);
 if($result){
-	header('location: loginpage.php');
+	header('location: index.php');
 }else{
 	echo $conn->error;
 }
