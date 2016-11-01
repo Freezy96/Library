d3f52ddb474ec4c3c9a41ba1827d5ffae3b4d5e5
Freezy96 <stenlleycold@yahoo.com>
2016-11-01 16:31:41 +0800

    merge conplete

--- adminnav.php
+++ adminnav.php
@@ -1,9 +1,5 @@
 <div class="list-group">
 	<a class="list-group-item active bg-danger" href="#"><span class="glyphicon glyphicon-th-list"></span> Admin Actions</a>
 	<a class="list-group-item" href="addinterface.php">Add Book</a>
-<<<<<<< HEAD
 	<a class="list-group-item" href="registerpage.php">Register for new Staff</a>
-=======
-	<a class="list-group-item" href="registerpage.php">Register new staff</a>
->>>>>>> origin/master
 </div>
