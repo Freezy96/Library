352ec227a89dfad30b64f0114ded5f86348ddb4b
botsking <botsking@gmail.com>
2016-11-01 14:14:58 +0800

    Register

--- adminnav.php
+++ adminnav.php
@@ -1,4 +1,5 @@
 <div class="list-group">
 	<a class="list-group-item active bg-danger" href="#"><span class="glyphicon glyphicon-th-list"></span> Admin Actions</a>
 	<a class="list-group-item" href="addinterface.php">Add Book</a>
+	<a class="list-group-item" href="registerpage.php">Register new staff</a>
 </div>
